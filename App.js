52498959fdd92f5c711960462f7192b4cd1807c5
Kavya <kavya.jain0102@gmail.com>
2023-02-04 12:20:55 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from
+    "react-navigation-material-bottom-tabs";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+  
+import HomeScreen from "./screens/HomeScreen";
+import InterestScreen from "./screens/InterestScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+  
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-home"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+    Interests: {
+      screen: InterestScreen,
+      navigationOptions: {
+        tabBarLabel: "Interests",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-settings-outline"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarLabel: "Profile",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-person-circle-outline"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: "Home",
+    barStyle: {backgroundColor: "#FA8128"},
+  }
+);
+  
+const Navigator = createAppContainer(TabNavigator);
+  
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Navigator>
+          <HomeScreen />
+      </Navigator>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
